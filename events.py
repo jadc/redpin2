5bcd1fe9be0326761dd832e68dab9a336522019d
jadc <chehimj@gmail.com>
2022-07-09 22:52:28 -0600

    20220709-225228

--- events.py
+++ events.py
@@ -12,7 +12,7 @@ class Events(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        print('---\n', payload)
+        #print('---\n', payload)
 
         if payload.guild_id is None:
             print('Reaction in DMs, ignored.')
